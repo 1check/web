8c89e4b9b8d7e36fbc756b983df000743ac66a1e
Omar Aflak <aflakomar@gmail.com>
2020-02-14 13:24:21 +0100

    more translations

--- src/assets/lang/en.js
+++ src/assets/lang/en.js
@@ -1,13 +1,15 @@
 const EN = 'EN';
 const data = {
+    app_name: 'OneCheck',
     // landing page
-    landing_catch_phrase: 'OneCheck YO, the smart way for business meals.',
+    landing_catch_phrase: 'OneCheck, the smart way for business meals.',
     // navigation
     navigation_sign_in: 'Sign In',
     navigation_sign_up: 'Sign Up',
     navigation_sign_out: 'Sign Out',
     navigation_account: 'Account',
     navigation_home: 'Home',
+    navigation_landing: 'Landing',
     // sign in page
     sign_in_title: 'Sign In',
     sign_in_email_label: 'Email address',
@@ -26,18 +28,24 @@ const data = {
     sign_up_confirm_password_label: 'Confirm password',
     sign_up_confirm_password_hint: 'Confirm password',
     sign_up_button: 'Sign Up',
+    sign_up_link_part1: 'Dont have an account?',
+    sign_up_link_part2: 'Sign Up',
     // password forget
     password_forget_title: 'Lost password',
     password_forget_email_label: 'Email address',
     password_forget_email_hint: 'Enter email',
     password_forget_recover_button: 'Recover password',
     password_forget_link: 'Forgot password?',
+    password_forget_done: 'Password recovery link sent!',
     // reset password
     password_change_password_label: 'New password',
     password_change_password_hint: 'Password',
     password_change_confirm_password_label: 'Confirm new password',
     password_change_confirm_password_hint: 'Confirm password',
     password_change_button: 'Reset password',
+    // footer
+    footer_contact_us: 'Contact us',
+    footer_about_us: 'About us'
 };
 
 export { EN };
