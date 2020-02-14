d02b1abcf25021f86d58e1ba9d053920594897b8
Omar Aflak <aflakomar@gmail.com>
2020-02-14 13:56:41 +0100

    lang drop down component

---
+++ src/components/LangDropDown/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Nav from 'react-bootstrap/Nav';
+
+import { ALL, getCurrentLang, setLang } from '../../assets/lang';
+
+const LangDropDown = () => {
+    const onItemSelected = (e1, event) => {
+        setLang(event.target.name);
+    };
+
+    const listItems = Object.keys(ALL).map(lang =>
+        <Dropdown.Item onSelect={onItemSelected} name={lang}>
+            {lang}
+        </Dropdown.Item>
+    );
+
+    return (
+        <Dropdown>
+            <Dropdown.Toggle as={Nav.Link}>🌍</Dropdown.Toggle>
+            <Dropdown.Menu>
+                {listItems}
+            </Dropdown.Menu>
+        </Dropdown>
+    );
+};
+
+export default LangDropDown;
