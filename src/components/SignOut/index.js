d8c3b593f6d0ec14e04976d3110286f893ee4214
Omar Aflak <aflakomar@gmail.com>
2020-02-16 16:33:24 +0100

    sign out button

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -7,7 +7,7 @@ import './index.css';
 import STRINGS from '../../assets/lang';
 
 const SignOutButton = ({ firebase }) => (
-    <Button variant="link" className="navigation-link sign-out-link font-weight-light" onClick={firebase.doSignOut}>{STRINGS.navigation_sign_out}</Button>
+    <Button variant="primary" className="navigation-link sign-out-link font-weight-light" onClick={firebase.doSignOut}>{STRINGS.navigation_sign_out}</Button>
 );
 
 export default withFirebase(SignOutButton);
