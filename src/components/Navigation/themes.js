e8c455ab22c844337ed5beac33e5edbb08940c7a
Omar Aflak <aflakomar@gmail.com>
2020-02-17 13:17:13 +0100

    added size as variable

--- src/components/Navigation/themes.js
+++ src/components/Navigation/themes.js
@@ -2,11 +2,15 @@ export default {
     dark: {
         '--color-navigation-link': 'white',
         '--color-navigation-background': 'transparent',
-        '--color-navigation-separator': 'transparent'
+        '--color-navigation-separator': 'transparent',
+        '--size-navigation-link': '20px',
+        '--size-navigation-separator': '1px'
     },
     light: {
         '--color-navigation-link': 'black',
         '--color-navigation-background': 'white',
-        '--color-navigation-separator': '#DCDCDC'
+        '--color-navigation-separator': '#DCDCDC',
+        '--size-navigation-link': '20px',
+        '--size-navigation-separator': '1px'
     }
 };
