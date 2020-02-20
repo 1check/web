e6c8667f89f523a0b094321b577a4d63d749a87b
Omar Aflak <aflakomar@gmail.com>
2020-02-20 22:05:55 +0100

    divider

--- src/components/Apply/index.js
+++ src/components/Apply/index.js
@@ -13,8 +13,9 @@ const ApplyPage = () => (
             {STRINGS.apply_text}
         </p>
         <div className="apply-links mt-5">
-            <Link className="p-5" to={ROUTES.APPLY_RESTAURANT}>{STRINGS.apply_restaurant}</Link>
-            <Link className="p-5" to={ROUTES.APPLY_COMPANY}>{STRINGS.apply_company}</Link>
+            <Link className="m-5" to={ROUTES.APPLY_RESTAURANT}>{STRINGS.apply_restaurant}</Link>
+            <div className="apply-divider border-left ml-5 mr-5"></div>
+            <Link className="m-5" to={ROUTES.APPLY_COMPANY}>{STRINGS.apply_company}</Link>
         </div>
     </Container>
 );
